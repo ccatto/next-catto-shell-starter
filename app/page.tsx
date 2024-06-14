ea6d93a5e0f552199ce8ab53cfb5d68f650bfd7e
Catto <catto@live.com>
2024-06-14 15:25:02 -0400

    simple working site.

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title:
+    "Chris Catto's Next starter",
+  description:
+    "Chris Catto's Next starter",
+};
+
+export default function Page() {
+  return (
+    <>
+      title
+    </>
+  );
+}
