20a0396e09996f30da9172bbf04a0f2e0c3dcb05
Catto <catto@live.com>
2024-06-18 17:07:41 -0400

    added nav.

--- app/components/NavCatto/NavCatto.tsx
+++ app/components/NavCatto/NavCatto.tsx
@@ -3,7 +3,12 @@ const NavCatto = () => {
   return (
     <>
       <nav className="fixed start-0 top-0 z-20 w-full border-b border-gray-200 dark:border-gray-600 dark:bg-gray-900 dark:text-white">
+        <div>
         <h1 className="dark:text-blue-400">Nav</h1>
+        </div>
+        <div>
+        <h1 className="dark:text-blue-400">Links</h1>
+        </div>
       </nav>
     </>
   );
